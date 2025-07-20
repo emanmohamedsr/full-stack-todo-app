9e9dedfeefa039fa7dc317564d56fabf3497de7f
Eman Mohamed <emanmohamedsr72@gmail.com>
2025-07-20 16:22:35 +0300

    add forward ref to input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,17 @@
-import type { InputHTMLAttributes } from "react";
+import { forwardRef, type InputHTMLAttributes, type Ref } from "react";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-const Input = (props: InputProps) => {
-	return (
-		<input
-			className='w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-800 text-gray-700 placeholder-gray-400'
-			{...props}
-		/>
-	);
-};
+const Input = forwardRef(
+	({ ...props }: InputProps, ref: Ref<HTMLInputElement>) => {
+		return (
+			<input
+				ref={ref}
+				className='w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-800 text-gray-700 placeholder-gray-400'
+				{...props}
+			/>
+		);
+	},
+);
 
 export default Input;
