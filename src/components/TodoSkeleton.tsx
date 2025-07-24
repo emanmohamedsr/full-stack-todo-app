fa1a00001b9050d1c9addecd5ec74b0de186cd2c
Eman Mohamed <emanmohamedsr72@gmail.com>
2025-07-24 17:33:33 +0300

    add description to todo skeleton

--- src/components/TodoSkeleton.tsx
+++ src/components/TodoSkeleton.tsx
@@ -1,15 +1,10 @@
 const TodoSkeleton = () => {
 	return (
-		<div
-			className=' w-full
-            bg-white p-4 rounded-lg
-            border border-gray-200
-            animate-pulse
-          '>
+		<div className='w-full bg-white p-4 rounded-lg border border-gray-200 animate-pulse'>
 			<div className='flex justify-between items-center'>
-				<div className='space-y-2 flex-1'>
+				<div className='space-y-3 flex-1'>
 					<div className='h-5 w-3/4 rounded bg-gray-200'></div>
-					<div className='h-3 w-1/2 rounded bg-gray-100'></div>
+					<div className='h-3 w-2/3 rounded bg-gray-100'></div>
 				</div>
 				<div className='flex space-x-2'>
 					<div className='h-8 w-16 rounded bg-gray-200'></div>
